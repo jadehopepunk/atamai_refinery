2a3502f71e5d1a26c5d0b6f704af74a334dc0614
Craig Ambrose <craig@craigambrose.com>
2011-06-08 20:12:31 +1200

    Initial refinery generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtamaiRefinery::Application.config.secret_token = '9944c4bb94dc0db7b6a2cd8b0834ee0125b4f289c6b2cf095251d64058437397946ff01772bcca55fe2d5da0297f715f00fae6f395e846f83184ff5866bdcd84'
