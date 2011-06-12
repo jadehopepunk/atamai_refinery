dc9af1e4a31e8a3ef1810f31b0e41437ee9fc5c0
Craig Ambrose <craig@craigambrose.com>
2011-06-12 20:59:47 +1200

    submenus work

--- app/helpers/sub_menu_helper.rb
+++ app/helpers/sub_menu_helper.rb
@@ -1,6 +1,17 @@
 module SubMenuHelper
   def menu_pages(current_page)
-    parent_page = current_page.root? ? current_page : current_page.parent
-    parent_page.children.live.in_menu
+    menu_parent_page(current_page).children.live.in_menu
   end
+  
+  private
+  
+    def menu_parent_page(current_page)
+      if current_page.root?
+        current_page
+      elsif current_page.children.empty? && !current_page.parent.root?
+        current_page.parent.parent
+      else
+        current_page.parent
+      end
+    end
 end
