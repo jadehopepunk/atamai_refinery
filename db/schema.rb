376bbacbc07919b505849ebcf8f1cf99e01dadca
Craig Ambrose <craig@craigambrose.com>
2011-12-14 20:23:01 +1300

    added anonymous role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111101080603) do
+ActiveRecord::Schema.define(:version => 20111210221939) do
 
   create_table "blog_categories", :force => true do |t|
     t.string   "title"
