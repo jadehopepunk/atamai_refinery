3c8d42fc0c5476809379c117590532b30a2d2bca
Craig Ambrose <craig@craigambrose.com>
2012-01-28 12:20:59 +1300

    More home page example pics. Testing auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :development_authentication
+
+  private
+  
+    def development_authentication
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "alt" && password == "tla"
+      end
+    end
 end
