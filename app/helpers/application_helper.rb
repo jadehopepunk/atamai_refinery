27ab58a5a7156abb1fd66ca5ba77b6514691ce5e
hailsonrails <doowelttil@gmail.com>
2015-08-06 14:30:46 +1200

    breadcrumbs enabled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,4 @@
 module ApplicationHelper
- 
-    def get_bread_crumb()
-      begin
-          url = request.path
-        breadcrumb = ''
-        elements = url.split('/')
-        for i in 1...elements.size - 1
-          #=  baseURL = "http://www.atamaivillage.org/" =
-          breadcrumb += elements[i]
-          breadcrumb += ' : ' if i != (elements.size - 2)
-        end
-        breadcrumb.titlecase
-      rescue
-        'Home'
-      end
-    end
     
     def get_title()
       begin
@@ -29,3 +13,47 @@ module ApplicationHelper
       end
     end
     
+    def get_bread_crumbOLD()
+      begin
+          url = request.path
+        breadcrumb = ''
+        elements = url.split('/')
+        for i in 1...elements.size - 1
+          #=  baseURL = "http://www.atamaivillage.org/" =
+          breadcrumb += elements[i]
+          breadcrumb += ' : ' if i != (elements.size - 2)
+        end
+        breadcrumb.titlecase
+      rescue
+        'Home'
+      end
+    end
+
+    
+    def get_bread_crumb()
+    begin
+      url = request.path
+        breadcrumb = ''
+        so_far = '/'
+        elements = url.split('/')
+        for i in 1...elements.size - 1
+        
+            so_far += elements[i] + '/'
+            
+            if elements[i] =~ /^\d+$/
+                begin
+                    breadcrumb += link_to_if(i != elements.size - 1, eval("#{elements[i - 1].singularize.camelize}.find(#{elements[i]}).name").gsub("_"," ").to_s, so_far)
+                rescue
+                    breadcrumb += elements[i]
+                end
+            else
+                breadcrumb += link_to_if(i != elements.size - 1,elements[i].gsub("_"," ").titleize, so_far)
+            end
+            
+            breadcrumb += " : " if i != elements.size - 2
+        end
+        breadcrumb.html_safe
+    rescue
+        'Not available'
+    end
+end
